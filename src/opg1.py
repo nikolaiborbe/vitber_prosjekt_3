8acade5f22aa6891d696e9faf6dc114e7058eac1
PetterTorj <petter.torjussen2@gmail.com>
2026-04-08 16:16:39 +0200

    Oppgave 1e ferdig og 1c oppdatert

--- src/opg1.py
+++ src/opg1.py
@@ -1,17 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Oppgave 1 c)
-
-alpha = 0.8
-tol = 1e-7
-y_init = np.array([0, 2])  # Initial conditions: y(0) = 0, y'(0) = 1
-x_init = 0
-x_end = 2 * np.pi
-h0 = 0.1
-
+# Oppgave 1c
 def f(x, y):
-    y_1, y_2 = y
+    y_1 = np.sin(2*x)
+    y_2 = 2 * np.cos(2*x)
     dy = [y_2, -4*y_1]
     return np.array(dy)
 
@@ -44,24 +37,57 @@ def BogackiShampine(x_init, x_end, y_init, f, h0, tol, alpha):
 
             X.append(x_n)
             Y.append(y_n.copy())
+            H.append(h)
             n_accept += 1
 
         else:
             n_reject += 1
 
         # oppdater steglengde
-        if est == 0:
-            h = 2*h
-        else:
-            h = alpha * h * (tol / est)**(1/3)
+        h = alpha * h * (tol / est)**(1/3)
             
-        stats = {
-            'n_accept': n_accept,
-            'n_reject': n_reject,
-            'final_step_size': h
-        }
+    stats = {
+        'n_accept': n_accept,
+        'n_reject': n_reject,
+        'step_length': h
+    }
 
-    return X, Y, H, stats
+    return np.array(X), np.array(Y), np.array(H), stats
+
+X, Y, H, stats = BogackiShampine(x_init, x_end, y_init, f, h0, tol, alpha)
+
+alpha = 0.8
+tol = 1e-3
+y_init = np.array([0, 2])
+x_init = 0
+x_end = 2 * np.pi
+h0 = 0.1
+
+# Plotting y(x) and y'(x)
+fig = plt.figure()
+ax1 = fig.add_subplot(121)
+ax2 = fig.add_subplot(122)
+
+ax1.plot(X, Y[:, 0], label='y(x)')
+ax1.plot(X, Y[:, 1], label="y'(x)")
+ax1.set_xlabel('x')
+ax1.set_ylabel('y(x)')
+ax1.set_title('y(x) and y\'(x) as function of x')
+ax1.grid()
+ax1.legend()
+
+# Plotting the step size h
+ax2.plot(X, H, label='Step length h')
+ax2.set_xlabel('x')
+ax2.set_ylabel('h')
+ax2.set_title('Step length varies as a function of x')
+ax2.grid() 
+ax2.legend() 
+
+plt.show()
+
+
+
 
 # Oppgave 1e
 def root_finder(func, guess1:float, guess2:float, tol:float):
