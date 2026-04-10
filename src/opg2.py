d7460a43d085b6ce873c197aa254ae54910327aa
PetterTorj <petter.torjussen2@gmail.com>
2026-04-10 14:13:23 +0200

    2d ferdig

--- src/opg2.py
+++ src/opg2.py
@@ -2,12 +2,6 @@ import numpy as np
 
 
 # Oppgave 2 a)
-
-# testvalue for matrix M
-M = np.array([[1 + 2j, 3 + 4j],
-              [6 + 9j, 7 + 8j]])
-
-
 def transform_matrix_to_vector(M):
     """
     Transforming a 2x2 complex matrix M into a real vector m.
@@ -26,9 +20,6 @@ def transform_matrix_to_vector(M):
 
     return np.array(m)
 
-m = transform_matrix_to_vector(M)
-print("Vector m:\n", m)
-
 
 def transform_vector_to_matrix(m):
     """
@@ -42,27 +33,13 @@ def transform_vector_to_matrix(m):
     M = real + 1j * imag
     return M.reshape((2, 2))
 
-M_reconstructed = transform_vector_to_matrix(m)
-print("Reconstructed Matrix M:\n", M_reconstructed)
-
-
 
 # Oppgave 2 b)
-
-# testvalue for four 8-component real vectors
-m1 = np.array([1, 2, 3, 4, 5, 6, 7, 8])
-m2 = np.array([9, 10, 11, 12, 13, 14, 15, 16])
-m3 = np.array([17, 18, 19, 20, 21, 22, 23, 24])
-m4 = np.array([25, 26, 27, 28, 29, 30, 31, 32])
-
 def transform_four_8comp_vectors_to_32comp_vector(m1, m2, m3, m4):
     """
     Transforming four 8-component real vectors into a single 32-component real vector v.
     """
     return np.concatenate((m1, m2, m3, m4))
-
-v = transform_four_8comp_vectors_to_32comp_vector(m1, m2, m3, m4)
-print("Vector v:\n", v)
 
 def transform_32comp_vector_to_four_8comp_vectors(v):
     """
@@ -74,25 +51,7 @@ def transform_32comp_vector_to_four_8comp_vectors(v):
     m4 = v[24:32]
     return m1, m2, m3, m4
 
-m1_reconstructed, m2_reconstructed, m3_reconstructed, m4_reconstructed = transform_32comp_vector_to_four_8comp_vectors(v)
-print("Reconstructed m1:", m1_reconstructed)
-print("Reconstructed m2:", m2_reconstructed)
-print("Reconstructed m3:", m3_reconstructed)
-print("Reconstructed m4:", m4_reconstructed)
-
-
-# Oppgave 2 c)
-
-# testvalue for unknown 2x2 complex matrices
-gamma = np.array([[1 + 2j, 3 + 4j],
-              [6 + 9j, 7 + 8j]])
-gamma_tilde = np.array([[5 + 6j, 7 + 8j],
-              [9 + 10j, 11 + 12j]])
-omega = np.array([[13 + 14j, 15 + 16j],
-              [17 + 18j, 19 + 20j]])
-omega_tilde = np.array([[21 + 22j, 23 + 24j],
-              [25 + 26j, 27 + 28j]])    
-
+# Oppgave 2 c) 
 def transform_matrices_to_32comp_vector(gamma, gamma_tilde, omega, omega_tilde):
     """
     Transforming four unknown 2x2 complex matrices into a single 32-component real vector v.
@@ -108,7 +67,6 @@ def transform_matrices_to_32comp_vector(gamma, gamma_tilde, omega, omega_tilde):
 
     return v
 
-print("Vector v:\n", transform_matrices_to_32comp_vector(gamma, gamma_tilde, omega, omega_tilde))
 
 def transform_32comp_vector_to_matrices(v):
     """
@@ -125,8 +83,36 @@ def transform_32comp_vector_to_matrices(v):
 
     return gamma, gamma_tilde, omega, omega_tilde
 
-gamma_reconstructed, gamma_tilde_reconstructed, omega_reconstructed, omega_tilde_reconstructed = transform_32comp_vector_to_matrices(transform_matrices_to_32comp_vector(gamma, gamma_tilde, omega, omega_tilde))
-print("Reconstructed gamma:\n", gamma_reconstructed)
-print("Reconstructed gamma_tilde:\n", gamma_tilde_reconstructed)
-print("Reconstructed omega:\n", omega_reconstructed)
-print("Reconstructed omega_tilde:\n", omega_tilde_reconstructed)
+# Exercise 2d
+def dv(v:np.ndarray, epsilon:float)->np.ndarray:
+    '''
+    Computes the derivative of the 32 component vector v wrt. x.
+    '''
+    gamma, gamma_tilde, omega, omega_tilde = transform_32comp_vector_to_matrices(v)
+
+    # Gamma and gamma_tilde have trivial derivatives
+    d_gamma, d_gamma_tilde = omega, omega_tilde
+
+    # The identity matrix
+    I = np.identity(2)
+
+    # Find N and N_tilde
+    N_inv = I - np.matmul(gamma, gamma_tilde)
+    N = np.linalg.inv(N_inv)
+
+    N_tilde_inv = I - np.matmul(gamma_tilde, gamma)
+    N_tilde = np.linalg.inv(N_tilde_inv)
+
+    # Multiply necessary matrices
+    product1 = np.matmul(omega, N_tilde)
+    product2 = np.matmul(gamma_tilde, omega)
+    product3 = np.matmul(omega_tilde, N)
+    product4 = np.matmul(gamma, omega_tilde)
+
+    # The derivatives of omega and omega_tidle
+    d_omega = -2j*(epsilon + 0.01j)*gamma - 2*np.matmul(product1, product2)
+    d_omega_tilde = -2j*(epsilon + 0.01j)*gamma_tilde - 2*np.matmul(product3, product4)
+
+    dv = transform_matrices_to_32comp_vector(d_gamma, d_gamma_tilde, d_omega, d_omega_tilde)
+
+    return dv
